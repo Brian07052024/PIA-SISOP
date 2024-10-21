a7f4cf4f3adef2299375e37af86e4239f52a6f7d
Brian07052024 <briancorpusramirez2@hotmail.com>
2024-10-20 23:05:27 -0600

    solucion errores

--- build/js/app.js
+++ build/js/app.js
@@ -6,6 +6,7 @@ function eventsListeners(){
     darkMode();
     menuHamburguesa();
     goTop();
+    recognizeStart();
 }
 
 function darkMode(){
@@ -22,7 +23,7 @@ function menuHamburguesa(){
     const enlaces = document.querySelector(".enlaces");
 
     menu.addEventListener("click", function(){
-        // console.log("diste click");
+        console.log("diste click");
         
         if(enlaces.classList.contains("mostrar")){
             enlaces.classList.remove("mostrar")
@@ -37,23 +38,34 @@ function menuHamburguesa(){
 }
 
 function goTop() {
-    const header = document.querySelector(".header");
+    const header = document.querySelector("#header");
     const upBtn = document.querySelector(".up-btn");
 
     window.addEventListener("scroll", function(){
         
-        if(window.innerWidth >= 768){
-            if(header.getBoundingClientRect().bottom < 0){
-                upBtn.classList.remove("ocultarBtn");
+        if(upBtn){
+            if(window.innerWidth >= 768){
+                if(header.getBoundingClientRect().bottom < 0){
+                    upBtn.classList.remove("ocultarBtn");
+                }else{
+                    upBtn.classList.add("ocultarBtn");
+                }  
             }else{
-                upBtn.classList.add("ocultarBtn");
-            }  
-        }else{
-             upBtn.classList.add("ocultarBtn");
+                 upBtn.classList.add("ocultarBtn");
+            }
         }
     })
-    
+}
 
-    
+function recognizeStart(){
+    const header = document.querySelector("header");
+
+    if(header.classList.contains("inicio")){
+        header.classList.add("header")
+        header.classList.remove("headerTwo")
+    }else{
+        header.classList.add("headerTwo")
+        header.classList.remove("header")
+    }
 }
 
