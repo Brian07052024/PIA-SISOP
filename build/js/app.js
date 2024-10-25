bac80b0acc7a43e402c3c4d934e047989b350365
Brian07052024 <briancorpusramirez2@hotmail.com>
2024-10-24 19:00:34 -0600

    apartado historia avanzaddo

--- build/js/app.js
+++ build/js/app.js
@@ -3,16 +3,17 @@ addEventListener("DOMContentLoaded", function(){
 })
 
 function eventsListeners(){
-    darkMode();
     menuHamburguesa();
-    goTop();
     recognizeStart();
+    textAnimation();
+    blinkEffect();
+    animations();
+    darkMode();
 }
 
 function darkMode(){
     const toggle = document.querySelector(".toggleMode");
     const body = document.querySelector("body");
-
     toggle.addEventListener("click", function(){
         body.classList.toggle("darkMode")
     })
@@ -21,45 +22,20 @@ function darkMode(){
 function menuHamburguesa(){
     const menu = document.querySelector(".menu");
     const enlaces = document.querySelector(".enlaces");
-
     menu.addEventListener("click", function(){
         console.log("diste click");
-        
         if(enlaces.classList.contains("mostrar")){
             enlaces.classList.remove("mostrar")
             
         }else{
             enlaces.classList.add("mostrar")
             
-        }
-    
-        
-    })
-}
-
-function goTop() {
-    const header = document.querySelector("#header");
-    const upBtn = document.querySelector(".up-btn");
-
-    window.addEventListener("scroll", function(){
-        
-        if(upBtn){
-            if(window.innerWidth >= 768){
-                if(header.getBoundingClientRect().bottom < 0){
-                    upBtn.classList.remove("ocultarBtn");
-                }else{
-                    upBtn.classList.add("ocultarBtn");
-                }  
-            }else{
-                 upBtn.classList.add("ocultarBtn");
-            }
-        }
+        }  
     })
 }
 
 function recognizeStart(){
     const header = document.querySelector("header");
-
     if(header.classList.contains("inicio")){
         header.classList.add("header")
         header.classList.remove("headerTwo")
@@ -69,3 +45,93 @@ function recognizeStart(){
     }
 }
 
+function textAnimation() {
+    const h1 = document.querySelector(".header-bottom");
+
+    if(h1){
+        const text = "Producto Integrador de aprendizaje: Linux";
+        let positionText = 0;
+
+        writeText();
+
+        function writeText() {
+            if (positionText < text.length) {
+                h1.textContent += text.charAt(positionText);
+                positionText++;
+                setTimeout(writeText, 35);
+            }
+        }
+    }
+
+    const bienvenida = document.querySelector(".bienvenida-cli");
+    if(bienvenida){
+        const text = "$ echo ¡Hola, $(Usuario)! Bienvenido a nuestra página web!"
+        let positionText = 0;
+
+        writeCli();
+
+        function writeCli(){
+            if(positionText < text.length){
+
+
+                bienvenida.textContent += text.charAt(positionText);
+                positionText++;
+                setTimeout(writeCli, 35);
+            }
+        }
+    }
+}
+
+function blinkEffect(){
+    const windowCliText = document.querySelectorAll(".window-cli_text");
+    windowCliText.forEach(e => {
+        e.classList.add("blinking")
+    });
+}
+
+function animations(){
+    const inicio = document.querySelector(".inicio");
+
+    if(inicio){
+        window.addEventListener("scroll", function(){
+            const bentoSection = document.querySelector(".bento-section");
+            if(bentoSection.getBoundingClientRect().top < 400 && bentoSection.getBoundingClientRect().bottom > 0){
+                if(!(bentoSection.classList.contains("bentoAnimation"))){
+                    bentoSection.classList.add("bentoAnimation")
+                }
+            }   
+        })
+    }
+
+    timeLine();
+    // const sectionCards = document.querySelector(".section-cards")
+    // const card = document.querySelectorAll(".card");
+
+    // if(sectionCards){
+    //     window.addEventListener("scroll", () => {
+    //         if(sectionCards.getBoundingClientRect().top < 0 && sectionCards.getBoundingClientRect().bottom > 0){
+    //             card.forEach(e => {
+    //                 e.classList.add("cards-animation");
+    //                 e.classList.remove("hidden");
+    //             });
+    //         }
+    //     })
+    // }
+
+    
+}
+
+function timeLine(){
+    const timeLineContent = document.querySelector(".timeLine");
+    
+
+    
+
+    window.addEventListener("scroll", function(){
+        const timeLineTop = timeLineContent.getBoundingClientRect().top;
+        if(timeLineTop < 400){
+            timeLineContent.classList.add("in-view")
+        }
+        
+    })
+}
